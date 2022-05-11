ca1346b65ece29026da18fcb0ccec80b302c105e
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2022-05-11 14:33:59 +0530

    Implement Order routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,15 @@
 import * as express from "express";
+import OrderController from "../controllers/OrderController";
 
 export default function setRoutes(app: any) {
   const router = express();
+  const orderControl = new OrderController();
 
   app.use("/api", router);
+  // Order Routes
+  router.route("/orders").post(orderControl.createOrder);
+  router.route("/orders").get(orderControl.getAllOrders);
+  router.route("/orders/:id").get(orderControl.getOrderById);
+  router.route("/orders/:id").put(orderControl.updateOrder);
+  router.route("/orders/:id").delete(orderControl.deleteOrder);
 }
